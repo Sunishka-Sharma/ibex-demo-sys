a860a5043b0e6257c08db29eb864da6ca3ce0945
Sunishka-Sharma <sunishka20137@iiitd.ac.in>
2024-04-25 01:40:26 +0530

    3% fixed- mexplicit make works

--- sw/c/gcc_toolchain.cmake
+++ sw/c/gcc_toolchain.cmake
@@ -1,28 +1,22 @@
 set(LINKER_SCRIPT "${CMAKE_CURRENT_LIST_DIR}/../common/link.ld")
 set(CMAKE_SYSTEM_NAME Generic)
-set(CMAKE_C_COMPILER riscv32-unknown-elf-gcc)
-set(CMAKE_OBJCOPY riscv32-unknown-elf-objcopy)
-set(CMAKE_C_FLAGS_INIT
-    "-march=rv32imc -mabi=ilp32 -mcmodel=medany -Wall -fvisibility=hidden -ffreestanding")
-set(CMAKE_ASM_FLAGS_INIT "-march=rv32imc")
-set(CMAKE_EXE_LINKER_FLAGS_INIT "-nostartfiles -T \"${LINKER_SCRIPT}\"")
-set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR riscv)
-#set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -march=rv32imc -mabi=ilp32 -mexplicit-relocs=none -mstrict-align -fno-common -mcmodel=medlow -mno-program-align -fvisibility=hidden -fno-pie -nostdlib")
-set(CMAKE_ASM_FLAGS "${CMAKE_ASM_FLAGS} -march=rv32imc -mabi=ilp32 -mexplicit-relocs=none -mstrict-align -fno-common -mcmodel=medlow -mno-program-align -fvisibility=hidden -fno-pie")
-set(CMAKE_EXE_LINKER_FLAGS "-nostartfiles -nostdlib -T ${LINKER_SCRIPT}")
 
+# Compiler and Toolchain Paths
 set(CMAKE_C_COMPILER /opt/homebrew/opt/riscv-gnu-toolchain/bin/riscv64-unknown-elf-gcc)
 set(CMAKE_CXX_COMPILER /opt/homebrew/opt/riscv-gnu-toolchain/bin/riscv64-unknown-elf-g++)
 set(CMAKE_ASM_COMPILER /opt/homebrew/opt/riscv-gnu-toolchain/bin/riscv64-unknown-elf-as)
 
-# Adjust these flags based on your specific target architecture
-set(CMAKE_C_FLAGS "-march=rv32imac -mabi=ilp32") # for 32-bit
-# or
-# set(CMAKE_C_FLAGS "-march=rv64imac -mabi=lp64") # for 64-bit
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -march=rv32imac_zicsr_zifencei")
+# Compiler Flags
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -march=rv32imac -mabi=ilp32 -fno-common -fno-pie -nostdlib")
 
+# Assembly Flags
+set(CMAKE_ASM_FLAGS "${CMAKE_ASM_FLAGS} -march=rv32imac -mabi=ilp32 -fno-common -fno-pie")
 
+# Linker Flags
+set(CMAKE_EXE_LINKER_FLAGS "-nostartfiles -nostdlib -T ${LINKER_SCRIPT}")
+
+# Find Root Path Modes
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
