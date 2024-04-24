c1eb94272870dd05f5af02e902890e7d7c870c5f
Sunishka-Sharma <sunishka20137@iiitd.ac.in>
2024-04-25 02:05:13 +0530

    fixed- nocommon 21% make works

--- sw/c/gcc_toolchain.cmake
+++ sw/c/gcc_toolchain.cmake
@@ -11,7 +11,7 @@ set(CMAKE_ASM_COMPILER /opt/homebrew/opt/riscv-gnu-toolchain/bin/riscv64-unknown
 set(CMAKE_C_FLAGS "-march=rv32imac_zicsr_zifencei -mabi=ilp32")
 
 # Assembly Flags
-set(CMAKE_ASM_FLAGS "${CMAKE_ASM_FLAGS} -march=rv64imac -mabi=lp64 -fno-common -fno-pie")
+set(CMAKE_ASM_FLAGS "-march=rv64imac -mabi=lp64")
 
 # Linker Flags
 set(CMAKE_EXE_LINKER_FLAGS "-nostartfiles -nostdlib -T ${LINKER_SCRIPT}")
