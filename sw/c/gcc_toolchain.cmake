736d26a646ce822845a0e5e908e79cfd8205ce28
Sunishka-Sharma <sunishka20137@iiitd.ac.in>
2024-04-25 02:01:54 +0530

    fixed- zicsr 21% make works

--- sw/c/gcc_toolchain.cmake
+++ sw/c/gcc_toolchain.cmake
@@ -8,10 +8,10 @@ set(CMAKE_CXX_COMPILER /opt/homebrew/opt/riscv-gnu-toolchain/bin/riscv64-unknown
 set(CMAKE_ASM_COMPILER /opt/homebrew/opt/riscv-gnu-toolchain/bin/riscv64-unknown-elf-as)
 
 # Compiler Flags
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -march=rv32imac -mabi=ilp32 -fno-common -fno-pie -nostdlib")
+set(CMAKE_C_FLAGS "-march=rv32imac_zicsr_zifencei -mabi=ilp32")
 
 # Assembly Flags
-set(CMAKE_ASM_FLAGS "${CMAKE_ASM_FLAGS} -march=rv32imac -mabi=ilp32 -fno-common -fno-pie")
+set(CMAKE_ASM_FLAGS "${CMAKE_ASM_FLAGS} -march=rv64imac -mabi=lp64 -fno-common -fno-pie")
 
 # Linker Flags
 set(CMAKE_EXE_LINKER_FLAGS "-nostartfiles -nostdlib -T ${LINKER_SCRIPT}")
