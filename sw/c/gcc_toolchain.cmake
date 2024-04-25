82b1aa939312d62bcfaf8d65cab7e4798b99d890
Sunishka-Sharma <sunishka20137@iiitd.ac.in>
2024-04-25 23:50:24 +0530

    fixed some ram error 100% commit

--- sw/c/gcc_toolchain.cmake
+++ sw/c/gcc_toolchain.cmake
@@ -8,14 +8,16 @@ set(CMAKE_CXX_COMPILER /opt/homebrew/opt/riscv-gnu-toolchain/bin/riscv64-unknown
 set(CMAKE_ASM_COMPILER /opt/homebrew/opt/riscv-gnu-toolchain/bin/riscv64-unknown-elf-as)
 set(CMAKE_C_FLAGS_INIT "-march=rv32imfc -mabi=ilp32f")
 set(CMAKE_ASM_FLAGS_INIT "-march=rv32imfc -mabi=ilp32f")
-# Compiler Flags
+
+# Assuming you are targeting a 32-bit architecture with these specific extensions
 set(CMAKE_C_FLAGS "-march=rv32imac_zicsr_zifencei -mabi=ilp32")
+set(CMAKE_ASM_FLAGS "-march=rv32imac_zicsr_zifencei -mabi=ilp32")
 
-# Assembly Flags
-set(CMAKE_ASM_FLAGS "-march=rv64imac -mabi=lp64")
 
-# Linker Flags
+# Ensure linker flags are correctly set
 set(CMAKE_EXE_LINKER_FLAGS "-nostartfiles -nostdlib -T ${LINKER_SCRIPT}")
+# Remove any -m flags here as they should be handled by the C and ASM flags if needed
+
 
 # Find Root Path Modes
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
