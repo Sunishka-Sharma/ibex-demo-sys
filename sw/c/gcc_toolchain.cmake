300294dee98ea12982e67a2f1d425c1733d7d843
Sunishka-Sharma <sunishka20137@iiitd.ac.in>
2024-04-26 01:34:24 +0530

    make works fully 100% rwx permission stuff warnings in linkerfile

--- sw/c/gcc_toolchain.cmake
+++ sw/c/gcc_toolchain.cmake
@@ -6,17 +6,16 @@ set(CMAKE_SYSTEM_PROCESSOR riscv)
 set(CMAKE_C_COMPILER /opt/homebrew/opt/riscv-gnu-toolchain/bin/riscv64-unknown-elf-gcc)
 set(CMAKE_CXX_COMPILER /opt/homebrew/opt/riscv-gnu-toolchain/bin/riscv64-unknown-elf-g++)
 set(CMAKE_ASM_COMPILER /opt/homebrew/opt/riscv-gnu-toolchain/bin/riscv64-unknown-elf-as)
-set(CMAKE_C_FLAGS_INIT "-march=rv32imfc -mabi=ilp32f")
-set(CMAKE_ASM_FLAGS_INIT "-march=rv32imfc -mabi=ilp32f")
+set(CMAKE_C_FLAGS_INIT "-march=rv32imac_zicsr_zifencei -mabi=ilp32")
+set(CMAKE_ASM_FLAGS_INIT "-march=rv32imac_zicsr_zifencei -mabi=ilp32")
 
-# Assuming you are targeting a 32-bit architecture with these specific extensions
 set(CMAKE_C_FLAGS "-march=rv32imac_zicsr_zifencei -mabi=ilp32")
 set(CMAKE_ASM_FLAGS "-march=rv32imac_zicsr_zifencei -mabi=ilp32")
 
 
+
 # Ensure linker flags are correctly set
-set(CMAKE_EXE_LINKER_FLAGS "-nostartfiles -nostdlib -T ${LINKER_SCRIPT}")
-# Remove any -m flags here as they should be handled by the C and ASM flags if needed
+set(CMAKE_EXE_LINKER_FLAGS "-nostartfiles -nostdlib -T ${LINKER_SCRIPT} -Wl,-O,elf32-littleriscv")
 
 
 # Find Root Path Modes
