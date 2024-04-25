aaf1e734edd215cbe8bcd0a3c8724ddd37ef55fa
Sunishka-Sharma <sunishka20137@iiitd.ac.in>
2024-04-25 11:57:55 +0530

    Updated CMakeLists.txt and main.c for LCD demo

--- sw/c/demo/lcd_st7735/main.c
+++ sw/c/demo/lcd_st7735/main.c
@@ -2,7 +2,7 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
-#include "core/lucida_console_10pt.h"
+#include "lucida_console_10pt.h"
 #include "demo_system.h"
 #include "fractal.h"
 #include "gpio.h"
